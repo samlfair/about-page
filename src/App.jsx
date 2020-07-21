bbdec6995ed64463c895f17cc8dfb8c62d7c4d61
Samuel Littlefair-Wallace <sam@littlefairwallace.ca>
2020-07-21 11:18:42 +0200

    Add API back in for testing

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,23 @@ import React, { useState, useEffect } from "react";
 import Prismic from "prismic-javascript";
 import "./App.css";
 import { Date, Link, RichText } from "prismic-reactjs";
-// import Slice from "./components/Slice";
-// import Header from "./components/Header";
-// const apiEndpoint = "https://blog-smile.cdn.prismic.io/api/v2";
-// const accessToken = process.env.REACT_APP_PRISMIC_ACCESS_TOKEN;
+import Slice from "./components/Slice";
+import Header from "./components/Header";
+const apiEndpoint = "https://blog-smile.cdn.prismic.io/api/v2";
+const accessToken = process.env.REACT_APP_PRISMIC_ACCESS_TOKEN;
 
-// const Client = Prismic.client(apiEndpoint, { accessToken });
+const Client = Prismic.client(apiEndpoint, { accessToken });
 
 function App() {
   const [doc, setDocData] = useState(null);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const response = await Client.getSingle(`about_page`);
-  //     setDocData(response.data);
-  //   };
-  //   fetchData();
-  // }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await Client.getSingle(`about_page`);
+      setDocData(response.data);
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className="app">
@@ -26,6 +26,7 @@ function App() {
         {!doc && <div id="loading">Loading...</div>}
         {doc && (
           <>
+            {JSON.stringify(doc)}
             {/* <Header title={doc.title} subheading={doc.subheading} /> */}
 
             {/* {doc.body.map((section) => {
