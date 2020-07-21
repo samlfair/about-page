c77779b5b31f1851f736beb3a234b85ce5dcca9d
Samuel Littlefair-Wallace <sam@littlefairwallace.ca>
2020-07-21 11:14:29 +0200

    Updates

---
+++ precache-manifest.4b1d1f5c4dc7fa4bb57ae4990f5ea1c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c48fa075be914b3febabc3ebb38c9e",
+    "url": "/about-page/index.html"
+  },
+  {
+    "revision": "39c335b0b2feeca552ec",
+    "url": "/about-page/static/css/main.a07ba9a6.chunk.css"
+  },
+  {
+    "revision": "b5494febec4df561835c",
+    "url": "/about-page/static/js/2.9f266d4f.chunk.js"
+  },
+  {
+    "revision": "6bd8b2c704173550ceb61ef17da441b4",
+    "url": "/about-page/static/js/2.9f266d4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39c335b0b2feeca552ec",
+    "url": "/about-page/static/js/main.cdde4185.chunk.js"
+  },
+  {
+    "revision": "b2c2fa1cc4f4ca11afa2",
+    "url": "/about-page/static/js/runtime-main.2738520a.js"
+  }
+]);
