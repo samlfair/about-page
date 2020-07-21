65960e8e4e552a0e65abb37de0efcf6926682a3b
Samuel Littlefair-Wallace <sam@littlefairwallace.ca>
2020-07-21 10:29:43 +0200

    Remove all components for testing

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import Prismic from "prismic-javascript";
 import "./App.css";
 import { Date, Link, RichText } from "prismic-reactjs";
-import Slice from "./components/Slice";
-import Header from "./components/Header";
+// import Slice from "./components/Slice";
+// import Header from "./components/Header";
 const apiEndpoint = "https://blog-smile.cdn.prismic.io/api/v2";
 const accessToken = process.env.REACT_APP_PRISMIC_ACCESS_TOKEN;
 
@@ -26,7 +26,7 @@ function App() {
         {!doc && <div id="loading">Loading...</div>}
         {doc && (
           <>
-            <Header title={doc.title} subheading={doc.subheading} />
+            {/* <Header title={doc.title} subheading={doc.subheading} /> */}
 
             {doc.body.map((section) => {
               if (section.slice_type) return <Slice slice={section} />;
