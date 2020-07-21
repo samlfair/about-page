5fe29d4f800acf37c7d938d77c2b0e487de8ba6e
Samuel Littlefair-Wallace <sam@littlefairwallace.ca>
2020-07-21 10:52:08 +0200

    Remove API for testing

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,21 @@ import "./App.css";
 import { Date, Link, RichText } from "prismic-reactjs";
 // import Slice from "./components/Slice";
 // import Header from "./components/Header";
-const apiEndpoint = "https://blog-smile.cdn.prismic.io/api/v2";
-const accessToken = process.env.REACT_APP_PRISMIC_ACCESS_TOKEN;
+// const apiEndpoint = "https://blog-smile.cdn.prismic.io/api/v2";
+// const accessToken = process.env.REACT_APP_PRISMIC_ACCESS_TOKEN;
 
-const Client = Prismic.client(apiEndpoint, { accessToken });
+// const Client = Prismic.client(apiEndpoint, { accessToken });
 
 function App() {
   const [doc, setDocData] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await Client.getSingle(`about_page`);
-      setDocData(response.data);
-    };
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await Client.getSingle(`about_page`);
+  //     setDocData(response.data);
+  //   };
+  //   fetchData();
+  // }, []);
 
   return (
     <div className="app">
